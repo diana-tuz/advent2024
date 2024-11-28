1437a1d28d0fe847147813a5d7a52d58d07eb0ff
Diana Tuz <dianatuz12@gmail.com>
2024-11-28 18:01:15 +0200

    Improve CodeEditor

--- src/components/CodeEditor/types.ts
+++ src/components/CodeEditor/types.ts
@@ -2,7 +2,7 @@ export interface CodeEditorPropsType {
   buttonTitle?: string;
   error: string;
   handleRunCode: () => void;
-  isUserData: boolean;
+  isUserData?: boolean;
   result: any[];
   setUserCode: (arg: string) => void;
   userCode: string;
