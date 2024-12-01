415b50b022d2f147f4159f2b0125ebff9edd2987
Diana Tuz <dianatuz12@gmail.com>
2024-12-01 20:26:18 +0200

    add analitics

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
+import { Analytics } from "@vercel/analytics/react";
 import { Home, Task } from "./pages";
-
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/tasks" element={<Task />} />
-      <Route path="*" element={<div>Page Not Found</div>} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tasks" element={<Task />} />
+        <Route path="*" element={<div>Page Not Found</div>} />
+      </Routes>
+      <Analytics />
+    </>
   );
 }
 
